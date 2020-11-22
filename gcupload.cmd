cb14709c6bd165f0ce73fd534852695dd5b3c0ff
flyinggorilla <flyinggorilla@outlook.com>
2020-11-22 14:11:33 +0100

    first commit

---
+++ gcupload.cmd
@@ -0,0 +1,35 @@
+call gsutil cp sailtwist.html gs://sailingbucket
+call gsutil acl ch -u AllUsers:R gs://sailingbucket/sailtwist.html
+
+call gsutil cp hull.stl gs://sailingbucket
+call gsutil acl ch -u AllUsers:R gs://sailingbucket/hull.stl
+
+call gsutil cp dat.gui.module.js gs://sailingbucket
+call gsutil acl ch -u AllUsers:R gs://sailingbucket/dat.gui.module.js
+
+call gsutil cp mast.stl gs://sailingbucket
+call gsutil acl ch -u AllUsers:R gs://sailingbucket/mast.stl
+
+call gsutil cp OrbitControls.js gs://sailingbucket
+call gsutil acl ch -u AllUsers:R gs://sailingbucket/OrbitControls.js
+
+call gsutil cp sailtwist.css gs://sailingbucket
+call gsutil acl ch -u AllUsers:R gs://sailingbucket/sailtwist.css
+
+call gsutil cp Sky.js gs://sailingbucket
+call gsutil acl ch -u AllUsers:R gs://sailingbucket/Sky.js
+
+call gsutil cp Water.js gs://sailingbucket
+call gsutil acl ch -u AllUsers:R gs://sailingbucket/Water.js
+
+call gsutil cp stats.module.js gs://sailingbucket
+call gsutil acl ch -u AllUsers:R gs://sailingbucket/stats.module.js
+
+call gsutil cp STLLoader.js gs://sailingbucket
+call gsutil acl ch -u AllUsers:R gs://sailingbucket/STLLoader.js
+
+call gsutil cp three.module.js gs://sailingbucket
+call gsutil acl ch -u AllUsers:R gs://sailingbucket/three.module.js
+
+call gsutil cp waternormals.jpg gs://sailingbucket
+call gsutil acl ch -u AllUsers:R gs://sailingbucket/waternormals.jpg
---
+++ sailtwist.js
@@ -0,0 +1,69 @@
+const https = require('https');
+const zlib = require('zlib');
+const express = require('express')
+
+const mode = process.env.NODE_ENV; // set to "production" when in prod
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+var listenPort = 3000;
+var isProduction = (mode == 'production');
+if (isProduction)
+    listenPort = process.env.PORT;
+
+console.log(mode)
+console.log("port: " + listenPort)
+
+const app = express();
+
+if (mode != 'production') {
+   var path = require('path');
+
+    app.get('/', function(rootreq, rootres) {
+        rootres.set("Access-Control-Allow-Origin", "*");
+        rootres.sendFile(path.join(__dirname + '/sailtwist.html'));
+    });
+
+    app.get('/favicon.png', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/favicon.png'));
+    });
+
+    app.get('/three.min.js', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/three.min.js'));
+    });
+    app.get('/STLLoader.js', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/STLLoader.js'));
+    });
+    app.get('/hull.stl', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/hull.stl'));
+    });
+    app.get('/stats.module.js', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/stats.module.js'));
+    });
+    app.get('/three.module.js', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/three.module.js'));
+    });
+    app.get('/dat.gui.module.js', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/dat.gui.module.js'));
+    });
+    app.get('/Water.js', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/Water.js'));
+    });
+    app.get('/Sky.js', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/Sky.js'));
+    });
+    app.get('/OrbitControls.js', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/OrbitControls.js'));
+    });
+    app.get('/sailtwist.css', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/sailtwist.css'));
+    });
+    app.get('/mast.stl', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/mast.stl'));
+    });
+    app.get('/waternormals.jpg', function(rootreq, rootres) {
+        rootres.sendFile(path.join(__dirname + '/waternormals.jpg'));
+    });
+
+}
+
+app.listen(listenPort, () => console.log(`Sailtwist app listening on port ${listenPort}!`))
+
