f53efc1899792712b5f993d1488ad8fefc5b4da6
flyinggorilla <flyinggorilla@outlook.com>
2020-11-22 14:55:51 +0100

    updates

--- gcupload.cmd
+++ gcupload.cmd
@@ -1,35 +1,35 @@
-call gsutil cp sailtwist.html gs://sailingbucket
-call gsutil acl ch -u AllUsers:R gs://sailingbucket/sailtwist.html
+call gsutil cp sailtwist.html gs://simulator.atterwind.info
+call gsutil acl ch -u AllUsers:R gs://simulator.atterwind.info/sailtwist.html
 
-call gsutil cp hull.stl gs://sailingbucket
-call gsutil acl ch -u AllUsers:R gs://sailingbucket/hull.stl
+call gsutil cp hull.stl gs://simulator.atterwind.info
+call gsutil acl ch -u AllUsers:R gs://simulator.atterwind.info/hull.stl
 
-call gsutil cp dat.gui.module.js gs://sailingbucket
-call gsutil acl ch -u AllUsers:R gs://sailingbucket/dat.gui.module.js
+call gsutil cp dat.gui.module.js gs://simulator.atterwind.info
+call gsutil acl ch -u AllUsers:R gs://simulator.atterwind.info/dat.gui.module.js
 
-call gsutil cp mast.stl gs://sailingbucket
-call gsutil acl ch -u AllUsers:R gs://sailingbucket/mast.stl
+call gsutil cp mast.stl gs://simulator.atterwind.info
+call gsutil acl ch -u AllUsers:R gs://simulator.atterwind.info/mast.stl
 
-call gsutil cp OrbitControls.js gs://sailingbucket
-call gsutil acl ch -u AllUsers:R gs://sailingbucket/OrbitControls.js
+call gsutil cp OrbitControls.js gs://simulator.atterwind.info
+call gsutil acl ch -u AllUsers:R gs://simulator.atterwind.info/OrbitControls.js
 
-call gsutil cp sailtwist.css gs://sailingbucket
-call gsutil acl ch -u AllUsers:R gs://sailingbucket/sailtwist.css
+call gsutil cp sailtwist.css gs://simulator.atterwind.info
+call gsutil acl ch -u AllUsers:R gs://simulator.atterwind.info/sailtwist.css
 
-call gsutil cp Sky.js gs://sailingbucket
-call gsutil acl ch -u AllUsers:R gs://sailingbucket/Sky.js
+call gsutil cp Sky.js gs://simulator.atterwind.info
+call gsutil acl ch -u AllUsers:R gs://simulator.atterwind.info/Sky.js
 
-call gsutil cp Water.js gs://sailingbucket
-call gsutil acl ch -u AllUsers:R gs://sailingbucket/Water.js
+call gsutil cp Water.js gs://simulator.atterwind.info
+call gsutil acl ch -u AllUsers:R gs://simulator.atterwind.info/Water.js
 
-call gsutil cp stats.module.js gs://sailingbucket
-call gsutil acl ch -u AllUsers:R gs://sailingbucket/stats.module.js
+call gsutil cp stats.module.js gs://simulator.atterwind.info
+call gsutil acl ch -u AllUsers:R gs://simulator.atterwind.info/stats.module.js
 
-call gsutil cp STLLoader.js gs://sailingbucket
-call gsutil acl ch -u AllUsers:R gs://sailingbucket/STLLoader.js
+call gsutil cp STLLoader.js gs://simulator.atterwind.info
+call gsutil acl ch -u AllUsers:R gs://simulator.atterwind.info/STLLoader.js
 
-call gsutil cp three.module.js gs://sailingbucket
-call gsutil acl ch -u AllUsers:R gs://sailingbucket/three.module.js
+call gsutil cp three.module.js gs://simulator.atterwind.info
+call gsutil acl ch -u AllUsers:R gs://simulator.atterwind.info/three.module.js
 
-call gsutil cp waternormals.jpg gs://sailingbucket
-call gsutil acl ch -u AllUsers:R gs://sailingbucket/waternormals.jpg
+call gsutil cp waternormals.jpg gs://simulator.atterwind.info
+call gsutil acl ch -u AllUsers:R gs://simulator.atterwind.info/waternormals.jpg
